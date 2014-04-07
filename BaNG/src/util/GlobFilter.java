81f1fd551f0ed4e46ef154e5ce7dd949f8bc5484
Bob Mathews <bobmath@sbcglobal.net>
2014-04-07 16:58:34 -0700

    ditch swing FileFilter

--- BaNG/src/util/GlobFilter.java
+++ BaNG/src/util/GlobFilter.java
@@ -4,25 +4,14 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.util.regex.Pattern;
 
-import javax.swing.filechooser.FileFilter;
-
-public class GlobFilter extends FileFilter implements FilenameFilter {
-	private String pattern;
+public class GlobFilter implements FilenameFilter {
 	private Pattern regex;
-	private boolean matchDirs;
 
 	public GlobFilter(String pattern) {
-		this.pattern = pattern;
-		regex = globToRegex(pattern);
+		regex = globToPattern(pattern);
 	}
 
-	public GlobFilter(String pattern, boolean dirs) {
-		this.pattern = pattern;
-		regex = globToRegex(pattern);
-		matchDirs = dirs;
-	}
-
-	public static Pattern globToRegex(String in) {
+	public static Pattern globToPattern(String in) {
 		StringBuilder out = new StringBuilder();
 		out.append('^');
 		for (int i = 0, len = in.length(); i < len; i++) {
@@ -41,24 +30,7 @@ public class GlobFilter extends FileFilter implements FilenameFilter {
 
 	@Override
 	public boolean accept(File dir, String name) {
-		if (matchDirs && new File(dir,name).isDirectory())
-			return true;
-		return accept(name);
-	}
-
-	@Override
-	public boolean accept(File f) {
-		if (matchDirs && f.isDirectory())
-			return true;
-		return accept(f.getName());
-	}
-
-	public boolean accept(String name) {
 		return regex.matcher(name).matches();
 	}
 
-	@Override
-	public String getDescription() {
-		return pattern;
-	}
 }
