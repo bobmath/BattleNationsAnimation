babd81f285530eb89131a569ea5a70473d619b03
Bob Mathews <bobmath@sbcglobal.net>
2014-04-16 15:42:17 -0700

    replace the huge default icon

--- BaNG/src/ui/AnimationGrabber.java
+++ BaNG/src/ui/AnimationGrabber.java
@@ -51,6 +51,8 @@ import bn.Unit.Weapon;
 
 public class AnimationGrabber {
 
+	public static final String VERSION = "1.0";
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
@@ -102,7 +104,7 @@ public class AnimationGrabber {
 	private JComboBox<String> busyCtrl;
 
 	public void buildUI() {
-		frame = new JFrame("AnimationGrabber");
+		frame = new JFrame("AnimationGrabber " + VERSION);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		tree = new JTree(AnimationTree.buildTree());
