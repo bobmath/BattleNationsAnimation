e94cb219463666ab6c63848fb70742886fc93744
Bob Mathews <bobmath11@icloud.com>
2014-04-25 15:28:58 -0700

    corners again

--- BaNG/src/bn/DamagePattern.java
+++ BaNG/src/bn/DamagePattern.java
@@ -41,17 +41,15 @@ public class DamagePattern implements Drawable {
 		boolean flip = pos < 0;
 		Ability abil = attack.getAbility();
 		GridPoint center = new GridPoint(0, pos + range);
-		int yMin, yMax;
-		boolean corners;
+		int yMin, yMax, yLim;
 		if ("Weapon".equals(abil.getTargetType())) {
-			corners = false;
 			yMin = Math.max(-attack.getMaxRange(), -5);
-			yMax = -1;
+			yLim = -5;
+			yMax = 0;
 			center = center.translate(0, -range + (flip ? 1 : -1));
 		}
 		else {
-			corners = true;
-			yMin = -2;
+			yLim = yMin = -2;
 			yMax = Math.min(attack.getMaxRange() - 1, 2);
 		}
 
@@ -80,8 +78,7 @@ public class DamagePattern implements Drawable {
 			int x = sq.getX();
 			int y = sq.getY();
 			if (y < yMin || y > yMax || x < -4 || x > 4) continue;
-			if (corners && (y == yMin || y == yMax)
-					&& (x == -4 || x == 4)) continue;
+			if ((y == yMin || y == yLim) && (x == -4 || x == 4)) continue;
 			if (flip) {
 				x = -x;
 				y = -y;
