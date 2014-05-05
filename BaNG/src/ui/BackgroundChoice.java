01cd83df8e03873c325ea789cf6c5db2dfadbc36
Bob Mathews <bobmath11@icloud.com>
2014-05-05 08:08:25 -0700

    load other background images

--- BaNG/src/ui/BackgroundChoice.java
+++ BaNG/src/ui/BackgroundChoice.java
@@ -1,13 +1,17 @@
 package ui;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import util.ImageCache;
 import bn.GameFiles;
@@ -16,89 +20,110 @@ public class BackgroundChoice {
 
 	public static BackgroundChoice[] getBackgrounds() {
 		List<BackgroundChoice> items = new ArrayList<BackgroundChoice>();
-		items.add(new BackgroundChoice("Default Background", TYPE_COLOR));
-		items.add(new BackgroundChoice("Black", 0, 0, 0));
-		items.add(new BackgroundChoice("Critter Tan", 0xdd, 0xcc, 0xaa));
-		items.add(new BackgroundChoice("Player/Civ Orange", 0xeb, 0x81, 0x00));
-		items.add(new BackgroundChoice("Raider Brown", 0xbb, 0x99, 0x66));
-		items.add(new BackgroundChoice("Rebel/Wolf Gray", 0xcc, 0xcc, 0xcc));
-		items.add(new BackgroundChoice("Sky Blue", 0x87, 0xce, 0xfa));
-		items.add(new BackgroundChoice("White", 0xff, 0xff, 0xff));
-		items.add(new BackgroundChoice("Other Color...", TYPE_CHOOSE));
+		items.add(new ColorBackground("Default Background"));
+		items.add(new ColorBackground("Black", 0, 0, 0));
+		items.add(new ColorBackground("Critter Tan", 0xdd, 0xcc, 0xaa));
+		items.add(new ColorBackground("Neutral Beige", 0xe0, 0xdc, 0xd0));
+		items.add(new ColorBackground("Player/Civ Orange", 0xeb, 0x81, 0x00));
+		items.add(new ColorBackground("Raider Brown", 0xbb, 0x99, 0x66));
+		items.add(new ColorBackground("Rebel/Wolf Gray", 0xcc, 0xcc, 0xcc));
+		items.add(new ColorBackground("Sky Blue", 0x87, 0xce, 0xfa));
+		items.add(new ColorBackground("White", 0xff, 0xff, 0xff));
+		items.add(new ChooseColor("Other Color..."));
 		for (File file : GameFiles.glob("BattleMap*.png")) {
 			String name = file.getName();
 			name = name.replaceFirst("(?i:\\.png)$", "");
 			name = name.replaceFirst("^(?i:battle)", "");
-			items.add(new BackgroundChoice(name, file));
+			items.add(new ImageBackground(name, file));
 		}
+		items.add(new ChooseImage("Other Image..."));
 		BackgroundChoice[] array = new BackgroundChoice[items.size()];
 		return items.toArray(array);
 	}
 
 	private String name;
-	private File file;
-	private Color color;
-	private int type;
 
-	private static final int TYPE_COLOR = 1;
-	private static final int TYPE_CHOOSE = 2;
-	private static final int TYPE_IMAGE = 3;
-
-	public void setBackground(AnimationBox animBox) {
-		switch (type) {
-		case TYPE_COLOR:
-			animBox.setBackgroundColor(color);
-			break;
-		case TYPE_CHOOSE:
-			Color c = JColorChooser.showDialog(animBox,
-					"Choose Background Color", color);
-			if (c != null) {
-				color = c;
-				animBox.setBackgroundColor(color);
-			}
-			break;
-		case TYPE_IMAGE:
-			try {
-				animBox.setBackgroundImage(ImageCache.read(file));
-			}
-			catch (IOException e) {
-				JOptionPane.showMessageDialog(null,
-						"Unable to load background",
-						"Error", JOptionPane.ERROR_MESSAGE);
-			}
-			break;
-		}
-	}
-
-	private BackgroundChoice(String name, int type) {
+	protected BackgroundChoice(String name) {
 		this.name = name;
-		this.type = type;
-		if (type == TYPE_CHOOSE)
-			color = Color.WHITE;
-	}
-
-	private BackgroundChoice(String name, File file) {
-		this.name = name;
-		this.file = file;
-		type = TYPE_IMAGE;
-	}
-
-	private BackgroundChoice(String name, int r, int g, int b) {
-		this.name = name;
-		this.color = new Color(r, g, b);
-		type = TYPE_COLOR;
-	}
-
-	public File getFile() {
-		return file;
-	}
-
-	public Color getColor() {
-		return color;
 	}
 
 	public String toString() {
 		return name;
+	}
+
+	public void setBackground(AnimationBox animBox) {
+	}
+
+	protected static class ColorBackground extends BackgroundChoice {
+		private Color color;
+		protected ColorBackground(String name) {
+			super(name);
+		}
+		protected ColorBackground(String name, int r, int g, int b) {
+			super(name);
+			this.color = new Color(r, g, b);
+		}
+		public void setBackground(AnimationBox animBox) {
+			animBox.setBackgroundColor(color);
+		}
+	}
+
+	protected static class ChooseColor extends BackgroundChoice {
+		private Color color;
+		protected ChooseColor(String name) {
+			super(name);
+			this.color = Color.WHITE;
+		}
+		public void setBackground(AnimationBox animBox) {
+			Color c = JColorChooser.showDialog(animBox,
+					"Choose Background Color", color);
+			if (c == null) return;
+			color = c;
+			animBox.setBackgroundColor(color);
+		}
+	}
+
+	protected static class ImageBackground extends BackgroundChoice {
+		private File file;
+		protected ImageBackground(String name, File file) {
+			super(name);
+			this.file = file;
+		}
+		public void setBackground(AnimationBox animBox) {
+			try {
+				animBox.setBackgroundImage(ImageCache.read(file));
+			}
+			catch (IOException e) {
+				showError(animBox);
+			}
+		}
+	}
+
+	protected static class ChooseImage extends BackgroundChoice {
+		protected ChooseImage(String name) {
+			super(name);
+		}
+		public void setBackground(AnimationBox animBox) {
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setFileFilter(new FileNameExtensionFilter("Images", "png", "gif", "jpg", "jpeg"));
+			fileChooser.setDialogTitle("Load Background");
+			int userOption = fileChooser.showOpenDialog(animBox);
+			if (userOption != JFileChooser.APPROVE_OPTION) return;
+			File img = fileChooser.getSelectedFile();
+			if (img == null) return;
+			try {
+				animBox.setBackgroundImage(ImageIO.read(img));
+			}
+			catch (IOException e) {
+				showError(animBox);
+			}
+		}
+	}
+
+	protected static void showError(Component comp) {
+		JOptionPane.showMessageDialog(comp,
+				"Unable to load background",
+				"Error", JOptionPane.ERROR_MESSAGE);
 	}
 
 }
