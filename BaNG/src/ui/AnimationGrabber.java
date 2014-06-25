d735d2ef94ad519efdfa0effec800c88cb850b01
Bob Mathews <bobmath11@icloud.com>
2014-06-25 14:29:17 -0700

    bump version

--- BaNG/src/ui/AnimationGrabber.java
+++ BaNG/src/ui/AnimationGrabber.java
@@ -51,7 +51,7 @@ import bn.Unit.Weapon;
 
 public class AnimationGrabber {
 
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.2";
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
