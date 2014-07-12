4e8c7e05410ca50cbc2c46ab7707142904d66643
Bob Mathews <bobmath11@icloud.com>
2014-07-12 07:10:14 -0700

    bunp

--- BaNG/src/ui/AnimationGrabber.java
+++ BaNG/src/ui/AnimationGrabber.java
@@ -52,7 +52,7 @@ import bn.Unit.Weapon;
 
 public class AnimationGrabber {
 
-	public static final String VERSION = "1.2.3";
+	public static final String VERSION = "1.3";
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
